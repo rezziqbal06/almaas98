01b6a706fc2b2b8658590c894a166bf70e86581f
FaridAhmadFadhilah <lilirohanah13@gmail.com>
2023-09-24 07:52:07 +0700

    UPDATE export laporan

---
+++ app/view/admin/laporan/home.php
@@ -0,0 +1,232 @@
+<div id="page-content">
+    <!-- Static Layout Header -->
+    <div class="content-header">
+        <div class="row" style="">
+            <div class="col-md-6">
+                <div class="btn-group">
+                    <button type="button" onclick="history.back()" class="btn btn-info btn-submit"><i class="fa fa-arrow-left icon-submit"></i> Kembali</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Content -->
+    <div class="card">
+
+        <div class="card-header">
+            <h6><strong><?= $this->getTitle() ?></strong></h6>
+        </div>
+
+        <div class="card-body">
+
+            <div class="row align-items-center">
+                <div class="col-md-3 form-group">
+                    <label for="start_date">Tanggal Mulai <span class="text-danger">*</span></label>
+                    <input type="text" name="start_date" id="start_date" class="form-control datepicker">
+                </div>
+                <div class="col-md-3 form-group">
+                    <label for="end_date">Tanggal Selesai <span class="text-danger">*</span></label>
+                    <input type="text" name="end_date" id="end_date" class="form-control datepicker">
+                </div>
+                <div class="col-md-3 form-group">
+                    <label for="kawasan">Kawasan <span class="text-danger">*</span></label>
+                    <select name="kawasan" id="kawasan" class="form-control">
+                        <option value="all">Semua Kawasan</option>
+                    </select>
+                </div>
+                <div class="col-md-3 mt-3"><button type="button" class="btn btn-primary" id="filter_handler">Filter</button></div>
+            </div>
+
+            <div id="response_container"></div>
+        </div>
+
+    </div>
+
+    <!-- Surveyon -->
+    <div class="card">
+        <div class="card-header">
+            <div class="row justify-content-between">
+                <div class="col-10">
+                    <h5>Surveyon</h5>
+                </div>
+                <div class="col-2 justify-content-end">
+                    <button type="button" data-tipe="table-surveyon" data-label="LIST SURVEYON" class="btn btn-success export-pdf">Export</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive" id="surveyon_res" style="display: none;">
+                <section id="table-surveyon">
+                    <table class="table table-vcenter table-hover dt-wow table-parent" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Kawasan</th>
+                                <th>Nama</th>
+                                <th>NIK</th>
+                                <th>Telepon</th>
+                            </tr>
+                        </thead>
+                        <tbody id="surveyon_tbody"></tbody>
+                    </table>
+                    <div id="count_surveyon_res"></div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Omset Perbulan -->
+    <div class="card">
+        <div class="card-header">
+            <div class="row justify-content-between">
+                <div class="col-10">
+                    <h5>Omset Perbulan</h5>
+                </div>
+                <div class="col-2 justify-content-end">
+                    <button type="button" data-tipe="table-omset" data-label="OMSET PERBULAN" class="btn btn-success export-pdf">Export</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive" id="omset_res" style="display: none;">
+                <section id="table-omset">
+                    <table class="table table-vcenter table-hover dt-wow table-parent" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Bulan</th>
+                                <th>Jumlah</th>
+                                <th>Omset</th>
+                            </tr>
+                        </thead>
+                        <tbody id="omset_tbody"></tbody>
+                    </table>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Unit Terbooking -->
+    <div class="card">
+        <div class="card-header">
+            <div class="row justify-content-between">
+                <div class="col-10">
+                    <h5>Unit Terbooking</h5>
+                </div>
+                <div class="col-2 justify-content-end">
+                    <button type="button" data-tipe="table-unit_terboking" data-label="UNIT TERBOOKING" class="btn btn-success export-pdf">Export</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive" id="unit_terboking_res" style="display: none;">
+                <section id="table-unit_terboking">
+                    <P>
+                    <table class="table table-vcenter table-hover dt-wow table-parent" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Kawasan</th>
+                                <th>Konsumen</th>
+                                <th>Lantai</th>
+                                <th>lb/lt</th>
+                                <th>Marketing</th>
+                                <th>Nomor</th>
+                                <th>Posisi</th>
+                                <th>Status</th>
+                                <th>Tanggal Pesan</th>
+                                <th>Tipe</th>
+                                <th>Total Harga</th>
+                                <th>Unit</th>
+                            </tr>
+                        </thead>
+                        <tbody id="unit_terboking_tbody"></tbody>
+                    </table>
+                    </P>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Unit Terjual -->
+    <div class="card">
+        <div class="card-header">
+            <div class="row justify-content-between">
+                <div class="col-10">
+                    <h5>Unit Terjual</h5>
+                </div>
+                <div class="col-2 justify-content-end">
+                    <button type="button" data-tipe="table-unit_terjual" data-label="UNIT TERJUAL" class="btn btn-success export-pdf">Export</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive" id="unit_terjual_res" style="display: none;">
+                <section id="table-unit_terjual">
+                    <table class="table table-vcenter table-hover dt-wow table-parent" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Kawasan</th>
+                                <th>Konsumen</th>
+                                <th>Lantai</th>
+                                <th>lb/lt</th>
+                                <th>Marketing</th>
+                                <th>Nomor</th>
+                                <th>Posisi</th>
+                                <th>Status</th>
+                                <th>Tanggal Pesan</th>
+                                <th>Tipe</th>
+                                <th>Total Harga</th>
+                                <th>Unit</th>
+                            </tr>
+                        </thead>
+                        <tbody id="unit_terjual_tbody"></tbody>
+                    </table>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Unit Tersedia -->
+    <div class="card">
+        <div class="card-header">
+            <div class="row justify-content-between">
+                <div class="col-10">
+                    <h5>Unit Tersedia</h5>
+                </div>
+                <div class="col-2 justify-content-end">
+                    <button type="button" data-tipe="table-unit_tersedia" data-label="UNIT TERSEDIA" class="btn btn-success export-pdf">Export</button>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive" id="unit_tersedia_res" style="display: none;">
+                <section id="table-unit_tersedia">
+                    <table class="table table-vcenter table-hover dt-wow table-parent" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Blok</th>
+                                <th>Harga</th>
+                                <th>Kawasan</th>
+                                <th>Lantai</th>
+                                <th>lb/lt</th>
+                                <th>Nomor</th>
+                                <th>Posisi</th>
+                                <th>Tipe</th>
+                                <th>Unit</th>
+                            </tr>
+                        </thead>
+                        <tbody id="unit_tersedia_tbody"></tbody>
+                    </table>
+                </section>
+            </div>
+        </div>
+    </div>
+
+
+</div>
---
+++ app/view/admin/laporan/home_bottom.php
@@ -0,0 +1,379 @@
+$('#start_date').val(moment().startOf('year').format('YYYY-MM-DD'))
+$('#end_date').val(moment().endOf('year').format('YYYY-MM-DD'))
+filter()
+$('.export-pdf').on('click', function(e){
+  var contents = $(`#${$(this).data('tipe')}`).html()
+  const newWindow = window.open("about:blank");
+  if (newWindow) {
+  const contentToPrint = `<html>
+
+  <head>
+    <style>
+      @page {
+        size: auto;
+        margin: 0;
+      }
+
+      @media print {
+        @page {
+          size: 210mm 297mm;
+          /* A4 */
+        }
+
+        .table th {
+          font-weight: bold;
+        }
+
+        .table tbody tr:hover {
+          background-color: #d1e0e0;
+        }
+
+        img {
+          height: 4rem;
+        }
+
+        .td-responsive {
+          width: 25% !important;
+        }
+
+        th,
+        td {
+          font-size: 0.85rem;
+          padding: 0.3rem;
+        }
+
+
+        hr {
+          height: 5px;
+          border-top: 1px solid black;
+          border-bottom: 1px solid black;
+          border-right: none;
+          border-left: none;
+        }
+
+        .table-parent {
+          border-collapse: separate;
+          border: 1px solid black;
+          border-left: none;
+          width: 100%;
+        }
+
+        .table-parent thead th {
+          border: 1px solid black;
+          border-right: none;
+          border-top: none;
+          border-bottom: none;
+          white-space: nowrap;
+          font-size: 0.65rem;
+        }
+
+        .table-parent tbody td {
+          border: 1px solid black;
+          border-right: none;
+          border-bottom: none;
+          font-size: 0.65rem;
+          text-align: left;
+        }
+
+        *{
+          font-family: courier, courier new, serif;
+        }
+      }
+    </style>
+  </head>
+
+  <body>
+    <table>
+      <tr>
+        <td rowspan="3" class="td-responsive">
+          <img src="<?= $this->cdn_url("media/logo.png") ?>" alt="main_logo">
+        </td>
+        <td>
+          AL-MAAS 98 RESIDENCE
+        </td>
+      </tr>
+      <tr>
+        <td> Perumahan Bumi Permai, Jalan Bengawan Solo 17, RT 09 RW 03, Kelurahan Kaliurang, Kecamatan Kebahagiaan, Kota Malang, Jawa Timur, 224352</td>
+      </tr>
+      <tr>
+        <td>081802803423</td>
+      </tr>
+    </table>
+    <p>
+      <hr>
+    </p>
+    <center>
+      <h3>${$(this).data('label') ?? ''}</h3>
+    </center>
+
+    <table>
+      <tr>
+        <td>Periode</td>
+        <td>: ${moment($('#start_date').val()).format('DD-MM-YYYY')} - ${moment($('#end_date').val()).format('DD-MM-YYYY')}</td>
+      </tr>
+      <tr>
+        <td>Kawasan</td>
+        <td>: ${$('#kawasan option:selected').text()}</td>
+      </tr>
+    </table>
+    <p>${contents}</p>
+
+    <p>
+    <table style="width: 100%;">
+      <tr>
+        <td></td>
+        <td style="width: 50%"></td>
+        <td rowspan="3" style="vertical-align: top;">${getNamaHari(moment().format('d'))}, ${moment().format('DD')} ${getNamaBulan(moment().format('M'))} ${moment().format('YYYY')}</td>
+      </tr>
+      <tr>
+        <td style="vertical-align: top;">
+          <center>Mengetahui</center>
+        </td>
+        <td style="height: 5rem;"></td>
+      </tr>
+      <tr>
+        <td>
+          <center>Yayat Hendrayana</center>
+        </td>
+      </tr>
+    </table>
+    </p>
+  </body>
+
+  </html>`;
+
+  newWindow.document.open();
+  newWindow.document.write(contentToPrint);
+  newWindow.document.close();
+
+  newWindow.print();
+  newWindow.close();
+  }
+})
+
+$('.datepicker').datepicker({format: 'yyyy-mm-dd'})
+
+$.get("<?= base_url("api_admin/pengaturan/kategori/") ?>").done(res => {
+  if(res.data && res.data.length > 0){
+    var kawasan = ''
+    res.data.map(val => {
+    kawasan += `<option value="${val[0] ?? '' }">${val[1] ?? ''}</option>`
+    })
+
+    $('#kawasan').append(kawasan)
+  }
+})
+
+$('#filter_handler').on('click', function(){
+  filter();
+})
+
+function filter(){
+  if(validate() == true){
+
+    var surveyon_res = ''
+    var omset_res = ''
+    var unit_terboking = ''
+    var unit_terjual = ''
+    var unit_tersedia = ''
+
+    $.get(`<?= base_url('api_admin/laporan/') ?>?sdate=${$('#start_date').val()}&edate=${$('#end_date').val()}${$('#kawasan option:selected').val() != "all" ? '&a_kategori_id='+$('#kawasan option:selected').val() : ''}`).done(res => {
+      console.log(res)
+      if(res.status == "200" && res.data){
+        $('#surveyon_tbody').empty()
+        $('#omset_tbody').empty()
+        $('#unit_terboking_tbody').empty()
+        $('#unit_terjual_tbody').empty()
+        $('#unit_tersedia_tbody').empty() 
+        
+        //surveyon
+        if(res.data.list_surveyon && res.data.list_surveyon.length > 0){
+          $('#surveyon_res').show()
+          var surveyon_tbody = ''
+          res.data.list_surveyon.map((val, idx) => {
+          surveyon_tbody += `
+          <tr>
+            <td>${idx + 1}</td>
+            <td>${val.kawasan ?? ''}</td>
+            <td>${val.nama ?? ''}</td>
+            <td>${val.nik ?? ''}</td>
+            <td>${val.telp ?? ''}</td>
+          </tr>
+          `
+          })
+          $('#surveyon_tbody').html(surveyon_tbody)
+        }
+
+        if(res.data.count_surveyon && res.data.count_surveyon.length > 0){
+          var count_serveyon_tbody = ''
+          res.data.count_surveyon.map(val => {
+            count_serveyon_tbody += `
+              <tr>
+                <td>Jumlah Kawasan <b>${val.kawasan}</b></td>
+                <td>: <b>${val.jumlah}</b></td>
+              </tr>
+            `
+          })
+          $('#count_surveyon_res').html(`<table>${count_serveyon_tbody}</table>`)
+        }
+
+        //omset
+        if(res.data.omset && res.data.omset.length > 0){
+          $('#omset_res').show()
+          var omset_tbody = ''
+          res.data.omset.map((val, idx) => {
+          omset_tbody += `
+          <tr>
+            <td>${idx + 1}</td>
+            <td>${val.bulan ?? ''}</td>
+            <td>${val.jumlah ?? ''}</td>
+            <td>Rp. ${val.omset ?? 0}</td>
+          </tr>
+          `
+          })
+          $('#omset_tbody').html(omset_tbody)
+        }
+
+        //unit terboking
+        if(res.data.unit_booking && res.data.unit_booking.length > 0){
+          $('#unit_terboking_res').show()
+
+          res.data.unit_booking.map((val, idx) => {
+          unit_terboking += `
+          <tr>
+            <td class="border-print">${idx + 1}</td>
+            <td class="border-print">${val.kawasan ?? ''}</td>
+            <td class="border-print">${val.konsumen ?? ''}</td>
+            <td class="border-print">${val.lantai ?? ''}</td>
+            <td class="border-print">${val.luas_bangunan ?? ' '}(m<sup>2</sup>)/${val.luas_tanah ?? ''}(m<sup>2</sup>) </td>
+            <td class="border-print">${val.marketing ?? ''}</td>
+            <td class="border-print">${val.nomor ?? ''}</td>
+            <td class="border-print">${val.posisi ?? ''}</td>
+            <td class="border-print">${val.status ?? ''}</td>
+            <td class="border-print">${val.tgl_pesan ?? ''}</td>
+            <td class="border-print">${val.tipe ?? ''}</td>
+            <td class="border-print">${val.total_harga ?? ''}</td>
+            <td class="border-print">${val.unit ?? ''}</td>
+          </tr>
+          `
+          })
+
+          $('#unit_terboking_tbody').html(unit_terboking)
+        }
+
+        //unit terjual
+        if(res.data.unit_terjual && res.data.unit_terjual.length > 0){
+          $('#unit_terjual_res').show()
+
+          res.data.unit_terjual.map((val, idx) => {
+          unit_terjual += `
+          <tr>
+            <td class="border-print">${idx + 1}</td>
+            <td class="border-print">${val.kawasan ?? ''}</td>
+            <td class="border-print">${val.konsumen ?? ''}</td>
+            <td class="border-print">${val.lantai ?? ''}</td>
+            <td class="border-print">${val.luas_bangunan ?? ' '}(m<sup>2</sup>)/${val.luas_tanah ?? ''}(m<sup>2</sup>) </td>
+            <td class="border-print">${val.marketing ?? ''}</td>
+            <td class="border-print">${val.nomor ?? ''}</td>
+            <td class="border-print">${val.posisi ?? ''}</td>
+            <td class="border-print">${val.status ?? ''}</td>
+            <td class="border-print">${val.tgl_pesan ?? ''}</td>
+            <td class="border-print">${val.tipe ?? ''}</td>
+            <td class="border-print">${val.total_harga ?? ''}</td>
+            <td class="border-print">${val.unit ?? ''}</td>
+          </tr>
+          `
+          })
+
+          $('#unit_terjual_tbody').html(unit_terjual)
+        }
+
+        //unit tersedia
+        if(res.data.unit_tersedia && res.data.unit_tersedia.length > 0){
+          $('#unit_tersedia_res').show()
+
+          res.data.unit_tersedia.map((val, idx) => {
+          unit_tersedia += `
+          <tr>
+            <td class="border-print">${idx + 1}</td>
+            <td class="border-print">${val.blok ?? ''}</td>
+            <td class="border-print">${val.harga ?? ''}</td>
+            <td class="border-print">${val.kawasan ?? ''}</td>
+            <td class="border-print">${val.lantai ?? ''}</td>
+            <td class="border-print">${val.luas_bangunan ?? ' '}(m<sup>2</sup>)/${val.luas_tanah ?? ''}(m<sup>2</sup>) </td>
+            <td class="border-print">${val.nomor ?? ''}</td>
+            <td class="border-print">${val.posisi ?? ''}</td>
+            <td class="border-print">${val.tipe ?? ''}</td>
+            <td class="border-print">${val.unit ?? ''}</td>
+          </tr>
+          `
+          })
+
+          $('#unit_tersedia_tbody').html(unit_tersedia)
+        }
+
+
+      }
+    })
+  }
+}
+
+function validate(){
+  var attr = [
+  {
+  name: '#start_date',
+  label: 'Tanggal Mulai',
+  },
+  {
+  name: '#end_date',
+  label: 'Tanggal Selesai',
+  },
+  {
+  name: '#kawasan',
+  label: 'Kawasan',
+  }
+  ]
+
+  var message = ''
+
+  attr.map(val => {
+    if($(val.name).val() == ""){
+      message += `<li class="text-sm font-weight-bold text-white">${val.label} Harus Diisi</li>`
+    }
+  })
+
+
+  if(message != ""){
+  $('#response_container').html(`<div class="alert alert-danger alert-dismissible fade show">
+    <ul>${message}</ul>
+  </div>
+  `)
+  return false;
+  }
+
+  return true
+}
+
+function getNamaHari(digit) {
+  const hari = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'];
+
+  if (digit >= 0 && digit <= 6) {
+    return hari[digit]; 
+  } else {
+    return 'Angka hari tidak valid' ; 
+  } 
+
+}
+
+function getNamaBulan(digit) {
+  const bulan = [
+    'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+    'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+  ];
+
+  if (digit >= 1 && digit <= 12) {
+    return bulan[digit - 1]; // Karena indeks array dimulai dari 0
+  } else {
+    return 'Angka bulan tidak valid';
+  }
+}
